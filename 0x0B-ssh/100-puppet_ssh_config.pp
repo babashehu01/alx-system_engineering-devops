f558e1a1fd329a7a568c1ad584f71d26936c73f8
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-10-08 00:19:23 +0100

    Making changes to our configuration file using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# using Puppet to make changes to our configuration file
+
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
