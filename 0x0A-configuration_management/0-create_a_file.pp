74443413f1f65cbf43c686c92189ea0db129ad1e
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2023-01-10 18:30:12 +0100

    Fixing requiremnet fail

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,8 +2,7 @@
 
 file { '/tmp/school':
   ensure  => file,
-  path    => '/tmp/school',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
