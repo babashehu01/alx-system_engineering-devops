e7af54e2e4858139ecb33043f8287aabd198f22e
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2023-02-11 03:16:40 +0100

    Getting data using API and saving it in CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,20 +4,58 @@ import requests
 import sys
 
 
-if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    user_id = int(sys.argv[1])
+#!/usr/bin/python3
+"""
+Using a REST API, for a given employee ID and returns information
+about his/her TODO list progress.
+"""
+import csv
+import requests
+import sys
 
-    """Getting username"""
-    req = "{}users/{}".format(url, user_id)
-    user = requests.get(req)
-    user = user.json()
-    username = user['username']
 
-    """Getting tiltle and todo status"""
-    req = "{}todos?userId={}".format(url, user_id)
-    tasks = requests.get(req)
-    tasks = tasks.json()
+if __name__ == "__main__":
 
-    for task in tasks:
-        print(f'"{user_id}","{username}","{task["completed"]}","{task["title"]}"')
+    id_user = int(sys.argv[1])
+
+    req_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos').json()
+    req_user = requests.get(
+        'https://jsonplaceholder.typicode.com/users').json()
+
+    csv_file = sys.argv[1] + '.csv'
+
+    with open(csv_file, mode='w') as export_csv_file:
+        file_to_export = csv.writer(
+            export_csv_file, delimiter=',',
+            quotechar='"', quoting=csv.QUOTE_ALL)
+
+        for i in req_user:
+            if id_user == i.get('id'):
+                USERNAME = i.get('username')
+
+        for j in req_todos:
+            USER_ID = j.get('userId')
+            TASK_COMPLETED_STATUS = j.get('completed')
+            TASK_TITLE = j.get('title')
+            if id_user == j.get('userId'):
+                file_to_export.writerow([
+                     USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE])
+
+
+#    url = 'https://jsonplaceholder.typicode.com/'
+#    user_id = int(sys.argv[1])
+#
+#    """Getting username"""
+#    req = "{}users/{}".format(url, user_id)
+#    user = requests.get(req)
+#    user = user.json()
+#    username = user['username']
+#
+#    """Getting tiltle and todo status"""
+#    req = "{}todos?userId={}".format(url, user_id)
+#    tasks = requests.get(req)
+#    tasks = tasks.json()
+#
+#    for task in tasks:
+#        print(f'"{user_id}","{username}","{task["completed"]}","{task["title"]}"')
