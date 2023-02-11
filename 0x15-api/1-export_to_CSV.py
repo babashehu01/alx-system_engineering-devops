da708f3f0518b380f1a1a745d78be17b33ec427f
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2023-02-11 02:51:53 +0100

    Getting data using API and saving it in CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Getting website data using API and save it as CSV
+"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_id = int(sys.argv[1])
+    
+    """Getting username"""
+    req = "{}users/{}".format(url, user_id)
+    user = requests.get(req)
+    user = user.json()
+    username = user['username']
+
+    """Getting tiltle and todo status"""
+    req = "{}todos?userId={}".format(url, user_id)
+    tasks = requests.get(req)
+    tasks = tasks.json()
+    
+    for task in tasks:
+        "2","Antonette","False","suscipit repellat esse quibusdam voluptatem incidunt"
+        print(f'"{user_id}","{username}","{task["completed"]}", "{task["title"]}"')
